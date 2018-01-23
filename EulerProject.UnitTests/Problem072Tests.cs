5d1ea87ee102f036e9ca519b7135cd27f4dc575d
ILya-Lev <lis2889@gmail.com>
2018-01-24 01:20:26 +0200

    Problem72 - not solved

---
+++ EulerProject.UnitTests/Problem072Tests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EulerProject.UnitTests
+{
+	[TestClass]
+	public class Problem072Tests
+	{
+		private Problem072 _problemSolver;
+
+		[TestInitialize]
+		public void Initializer()
+		{
+			_problemSolver = new Problem072();
+		}
+
+		[DataRow(12, 3)]
+		[DataRow(12, 4)]
+		[DataRow(12, 6)]
+		[DataRow(12, 2)]
+		[DataRow(125, 25)]
+		[DataTestMethod]
+		public void HCF_MultiplicitNumbers_FindsSmaller(int a, int b)
+		{
+			var hcf = _problemSolver.HigestCommonFactor(a, b);
+			hcf.Should().Be(b);
+			Debug.Print($"a={a}; b={b}; hcf={hcf}");
+		}
+
+		[DataRow(12, 5)]
+		[DataRow(11, 4)]
+		[DataRow(21, 16)]
+		[DataRow(100, 49)]
+		[DataTestMethod]
+		public void HCF_RelativelyPrime_One(int a, int b)
+		{
+			var hcf = _problemSolver.HigestCommonFactor(a, b);
+			hcf.Should().Be(1);
+			Debug.Print($"a={a}; b={b}; hcf={hcf}");
+		}
+
+		[DataRow(12, 10, 2)]
+		[DataRow(24, 18, 6)]
+		[DataRow(100, 60, 20)]
+		[DataTestMethod]
+		public void HCF_StandardFlow_FindRightValue(int a, int b, int expected)
+		{
+			var hcf = _problemSolver.HigestCommonFactor(a, b);
+			hcf.Should().Be(expected);
+			Debug.Print($"a={a}; b={b}; hcf={hcf}");
+		}
+
+		[DataRow(2, 1)]
+		[DataRow(3, 2)]
+		[DataRow(4, 3)]
+		[DataRow(5, 4)]
+		[DataTestMethod]
+		public void FractionsForDenumerator_GenerateAmount(int denumerator, int amount)
+		{
+			var fractions = _problemSolver.FractionsForDenumerator(denumerator).ToList();
+			fractions.Count.Should().Be(amount);
+			Debug.Print(string.Join("; ", fractions.Select(f => $"{f.Numerator}/{f.Denumerator}")));
+		}
+
+		[TestMethod]
+		public void UniqueFractionsWithDenumeratorLessThan_8_21Fraction()
+		{
+			var fractions = _problemSolver.UniqueFractionsWithDenumeratorLessThan(8).ToList();
+			fractions.Should().HaveCount(21);
+			Debug.Print(string.Join("; ", fractions.Select(f => $"{f.Numerator}/{f.Denumerator}")));
+		}
+
+		[TestMethod]
+		public void UniqueFractionsWithDenumeratorLessThan_million_ItDepends()
+		{
+			var amount = _problemSolver.UniqueFractionsWithDenumeratorLessThan(1000).Count();
+			amount.Should().BeGreaterThan(21);
+			Debug.Print($"{amount}");
+		}
+	}
+}
---
+++ EulerProject/Problem072.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace EulerProject
+{
+	public class Problem072
+	{
+		internal int HigestCommonFactor(int a, int b)
+		{
+			if (a < b)
+			{
+				throw new ArgumentException($"{nameof(HigestCommonFactor)}'s first parameter should be bigger");
+			}
+
+			if (a == 1 || b == 1) return 1;
+
+			if (a % b == 0) return b;
+
+			for (int divisor = 2; divisor <= Math.Sqrt(a) && divisor < b; divisor++)
+			{
+				if (a % divisor == 0 && b % divisor == 0)
+					return divisor * HigestCommonFactor(a / divisor, b / divisor);
+			}
+
+			return 1;
+		}
+
+		internal Fraction Reduce(Fraction initial)
+		{
+			var hcf = HigestCommonFactor(initial.Denumerator, initial.Numerator);
+			return new Fraction
+			{
+				Numerator = initial.Numerator / hcf,
+				Denumerator = initial.Denumerator / hcf
+			};
+		}
+
+		internal IEnumerable<Fraction> FractionsForDenumerator(int denumerator)
+		{
+			return Enumerable
+				.Range(1, denumerator - 1)
+				.Select(numerator => new Fraction { Numerator = numerator, Denumerator = denumerator })
+				.Select(Reduce);
+		}
+
+		public IEnumerable<Fraction> UniqueFractionsWithDenumeratorLessThan(int limit)
+		{
+			return Enumerable
+				.Range(2, limit - 1)
+				.SelectMany(FractionsForDenumerator)
+				.Distinct(new FractionComparer());
+		}
+	}
+
+	[DebuggerDisplay("{Numerator}/{Denumerator}")]
+	public class Fraction : IEquatable<Fraction>
+	{
+		public int Numerator { get; set; }
+		public int Denumerator { get; set; }
+		public bool Equals(Fraction other)
+		{
+			return this.Numerator == other?.Numerator && this.Denumerator == other.Denumerator;
+		}
+	}
+
+	public class FractionComparer : IEqualityComparer<Fraction>
+	{
+		public bool Equals(Fraction x, Fraction y)
+		{
+			if (x == null && y == null)
+				return true;
+			return x?.Equals(y) ?? false;
+		}
+
+		public int GetHashCode(Fraction obj)
+		{
+			return obj.Numerator.GetHashCode() ^ obj.Denumerator.GetHashCode() ^ 31;
+		}
+	}
+}
