97de090d557985d4b32890b00151c7550978eaa5
ILya Lev <lis2889@gmail.com>
2018-01-25 10:17:07 +0200

    new approach with storing all prime numbers less than the threshold in an array; solved 072

--- EulerProject.UnitTests/Problem072Tests.cs
+++ EulerProject.UnitTests/Problem072Tests.cs
@@ -1,7 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Diagnostics;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Diagnostics;
-using System.Linq;
 
 namespace EulerProject.UnitTests
 {
@@ -24,7 +23,7 @@ namespace EulerProject.UnitTests
 		[DataTestMethod]
 		public void HCF_MultiplicitNumbers_FindsSmaller(int a, int b)
 		{
-			var hcf = _problemSolver.HigestCommonFactor(a, b);
+			var hcf = _problemSolver.HighestCommonFactor(a, b);
 			hcf.Should().Be(b);
 			Debug.Print($"a={a}; b={b}; hcf={hcf}");
 		}
@@ -36,7 +35,7 @@ namespace EulerProject.UnitTests
 		[DataTestMethod]
 		public void HCF_RelativelyPrime_One(int a, int b)
 		{
-			var hcf = _problemSolver.HigestCommonFactor(a, b);
+			var hcf = _problemSolver.HighestCommonFactor(a, b);
 			hcf.Should().Be(1);
 			Debug.Print($"a={a}; b={b}; hcf={hcf}");
 		}
@@ -47,7 +46,7 @@ namespace EulerProject.UnitTests
 		[DataTestMethod]
 		public void HCF_StandardFlow_FindRightValue(int a, int b, int expected)
 		{
-			var hcf = _problemSolver.HigestCommonFactor(a, b);
+			var hcf = _problemSolver.HighestCommonFactor(a, b);
 			hcf.Should().Be(expected);
 			Debug.Print($"a={a}; b={b}; hcf={hcf}");
 		}
@@ -59,24 +58,22 @@ namespace EulerProject.UnitTests
 		[DataTestMethod]
 		public void FractionsForDenumerator_GenerateAmount(int denumerator, int amount)
 		{
-			var fractions = _problemSolver.FractionsForDenumerator(denumerator).ToList();
-			fractions.Count.Should().Be(amount);
-			Debug.Print(string.Join("; ", fractions.Select(f => $"{f.Numerator}/{f.Denumerator}")));
+			var fractions = _problemSolver.FractionsForDenumerator(denumerator);
+			fractions.Should().Be(amount);
 		}
 
 		[TestMethod]
 		public void UniqueFractionsWithDenumeratorLessThan_8_21Fraction()
 		{
-			var fractions = _problemSolver.UniqueFractionsWithDenumeratorLessThan(8).ToList();
-			fractions.Should().HaveCount(21);
-			Debug.Print(string.Join("; ", fractions.Select(f => $"{f.Numerator}/{f.Denumerator}")));
+			var fractions = _problemSolver.AmountOfUniqueFractionsWithDenumeratorUpTo(8);
+			fractions.Should().Be(21);
 		}
 
 		[TestMethod]
 		public void UniqueFractionsWithDenumeratorLessThan_Thousand()
 		{
-			var amount = _problemSolver.UniqueFractionsWithDenumeratorLessThan(1000).Count();
-			amount.Should().BeGreaterOrEqualTo(306324);
+			var amount = _problemSolver.AmountOfUniqueFractionsWithDenumeratorUpTo(1000);
+			amount.Should().Be(306324);
 			Debug.Print($"{amount}");
 		}
 
@@ -84,7 +81,83 @@ namespace EulerProject.UnitTests
 		public void UniqueFractionsWithDenumeratorLessThan_TenThousands()
 		{
 			//30464963
-			var amount = _problemSolver.UniqueFractionsWithDenumeratorLessThan(1000 * 10).Count();
+			//30472088
+			var amount = _problemSolver.AmountOfUniqueFractionsWithDenumeratorUpTo(1000 * 10);
+			amount.Should().Be(30464963);
+			Debug.Print($"{amount}");
+		}
+
+		[TestMethod]
+		public void UniqueFractionsWithDenumeratorLessThan_HundredThousands()
+		{
+			//3177815551 is it correct? 13:49 on laptop
+			var amount = _problemSolver.AmountOfUniqueFractionsWithDenumeratorUpTo(1000 * 100);
+			amount.Should().BeGreaterOrEqualTo(306324);
+			Debug.Print($"{amount}");
+		}
+
+		[TestMethod]
+		public void UniqueFractionsWithDenumeratorLessThan_Million()
+		{
+			//3177815551 is it correct? 13:49 on laptop
+			var amount = _problemSolver.AmountOfUniqueFractionsWithDenumeratorUpTo(1000 * 1000);
+			amount.Should().BeGreaterOrEqualTo(306324);
+			Debug.Print($"{amount}");
+		}
+	}
+	[TestClass]
+	public class Problem072_1Tests
+	{
+		private Problem072_1 _problemSolver;
+
+		[TestInitialize]
+		public void Initializer()
+		{
+			_problemSolver = new Problem072_1();
+		}
+
+		[TestMethod]
+		public void FractionAmountUntil_8_21Fraction()
+		{
+			var fractions = _problemSolver.FractionAmountUntil(8);
+			fractions.Should().Be(21);
+		}
+
+		[TestMethod]
+		public void FractionAmountUntil_Thousand()
+		{
+			var amount = _problemSolver.FractionAmountUntil(1000);
+			amount.Should().Be(304191L);
+			Debug.Print($"{amount}");
+		}
+
+		[TestMethod]
+		public void FractionAmountUntil_TenThousands()
+		{
+			//30464963
+			//30472088
+			var amount = _problemSolver.FractionAmountUntil(1000 * 10);
+			amount.Should().Be(30397485L);
+			Debug.Print($"{amount}");
+		}
+
+		[TestMethod]
+		public void FractionAmountUntil_HundredThousands()
+		{
+			//3039650753 is it correct? 05:54 on laptop
+			var amount = _problemSolver.FractionAmountUntil(1000 * 100);
+			amount.Should().Be(3039650753);
+			Debug.Print($"{amount}");
+		}
+		/// <summary>
+		/// result is 303963552391 and it's correct!
+		/// took a few hours and 21 min 39.24 sec on laptop with i5 but less than 14 hours
+		/// </summary>
+		[TestMethod]
+		public void FractionAmountUntil_Million()
+		{
+			//3177815551 is it correct? 13:49 on laptop
+			var amount = _problemSolver.FractionAmountUntil(1000 * 1000);
 			amount.Should().BeGreaterOrEqualTo(306324);
 			Debug.Print($"{amount}");
 		}
--- EulerProject/Problem072.cs
+++ EulerProject/Problem072.cs
@@ -7,11 +7,12 @@ namespace EulerProject
 {
 	public class Problem072
 	{
-		internal int HigestCommonFactor(int a, int b)
+		#region legacy implementations
+		internal int HighestCommonFactor(int a, int b)
 		{
 			if (a < b)
 			{
-				throw new ArgumentException($"{nameof(HigestCommonFactor)}'s first parameter should be bigger");
+				throw new ArgumentException($"{nameof(HighestCommonFactor)}'s first parameter should be bigger");
 			}
 
 			if (a == 1 || b == 1) return 1;
@@ -21,37 +22,83 @@ namespace EulerProject
 			for (int divisor = 2; divisor <= Math.Sqrt(a) && divisor < b; divisor++)
 			{
 				if (a % divisor == 0 && b % divisor == 0)
-					return divisor * HigestCommonFactor(a / divisor, b / divisor);
+					return divisor * HighestCommonFactor(a / divisor, b / divisor);
 			}
 
 			return 1;
 		}
+		internal bool IsCrossPrime(int a, int b, double sqrtOfA)
+		{
+			if (a % b == 0) return b == 1;
+
+			var divisorLimit = Math.Min(sqrtOfA, b);
+			for (int divisor = 2; divisor <= divisorLimit; divisor++)
+			{
+				if (a % divisor == 0 && b % divisor == 0)
+					return false;
+			}
+
+			return true;
+		}
 
 		internal Fraction Reduce(Fraction initial)
 		{
-			var hcf = HigestCommonFactor(initial.Denumerator, initial.Numerator);
+			var hcf = HighestCommonFactor(initial.Denumerator, initial.Numerator);
 			return new Fraction
 			{
 				Numerator = initial.Numerator / hcf,
 				Denumerator = initial.Denumerator / hcf
 			};
 		}
+		#endregion
 
-		internal IEnumerable<Fraction> FractionsForDenumerator(int denumerator)
+		private IEnumerable<int> GetDenumeratorDividers(int denumerator, int sqrtOfDenumerator)
 		{
+			for (int divider = 2; divider <= sqrtOfDenumerator; divider++)
+			{
+				if (denumerator % divider == 0)
+				{
+					yield return divider;
+					var complementarityDivider = denumerator / divider;
+					if (complementarityDivider != divider)
+						yield return complementarityDivider;
+				}
+			}
+		}
+
+		internal bool IsCrossPrime(IReadOnlyDictionary<int, int> denumeratorDividers, int numerator)
+		{
+			if (denumeratorDividers.ContainsKey(numerator))
+				return false;
+
+			return denumeratorDividers
+				.TakeWhile(div => div.Key < numerator)
+				.All(div => numerator % div.Value != 0);
+		}
+
+		internal long FractionsForDenumerator(int denumerator)
+		{
+			var sqrtOfDenumerator = (int)Math.Sqrt(denumerator);
+			var denumeratorDividers = GetDenumeratorDividers(denumerator, sqrtOfDenumerator).ToDictionary(div => div);
+
+			if (denumeratorDividers.Count == 0)
+				return denumerator - 1;
+
 			return Enumerable
-				.Range(1, denumerator - 1)
-				.Select(numerator => new Fraction { Numerator = numerator, Denumerator = denumerator })
-				.Where(f => HigestCommonFactor(f.Denumerator, f.Numerator) == 1);
+				.Range(2, denumerator - 1)
+				.Count(numerator => IsCrossPrime(denumeratorDividers, numerator)) + 1;
+			//.Select(numerator => new Fraction { Numerator = numerator, Denumerator = denumerator })
+			//.Where(f => HighestCommonFactor(f.Denumerator, f.Numerator) == 1);
 			//.Select(Reduce);
 		}
 
-		public IEnumerable<Fraction> UniqueFractionsWithDenumeratorLessThan(int limit)
+		public long AmountOfUniqueFractionsWithDenumeratorUpTo(int limit)
 		{
 			return Enumerable
 				.Range(2, limit - 1)
 				.AsParallel()
-				.SelectMany(FractionsForDenumerator);
+				.Select(FractionsForDenumerator)
+				.Sum();
 			//.Distinct(new FractionComparer());
 		}
 	}
@@ -79,6 +126,70 @@ namespace EulerProject
 		public int GetHashCode(Fraction obj)
 		{
 			return obj.Numerator.GetHashCode() ^ obj.Denumerator.GetHashCode() ^ 31;
+		}
+	}
+
+	public class Problem072_1
+	{
+		public long FractionAmountUntil(int biggestDenumerator)
+		{
+			var primes = PrimesLessThan(biggestDenumerator).ToDictionary(p => p);
+
+			var amounts = Enumerable
+				.Range(2, biggestDenumerator - 1)
+				.AsParallel()
+				.Select(denumerator => FractionAmountFor(primes, denumerator))
+				.ToList();
+
+			return amounts.Sum();
+		}
+
+		private long FractionAmountFor(Dictionary<int, int> primes, int denumerator)
+		{
+			if (primes.ContainsKey(denumerator))
+			{
+				return denumerator - 1;
+			}
+
+			var amountOfCrossPrime = 1;
+			var dividers = DividersOf(denumerator, primes).ToList();
+			for (int numerator = 2; numerator < denumerator; numerator++)
+			{
+				if (dividers.All(d => numerator % d != 0))
+					amountOfCrossPrime++;
+			}
+			//var amountOfCrossPrime = Enumerable.Range(2, denumerator - 2)
+			//									.Count(numerator => dividers.All(d => numerator % d != 0));
+			return amountOfCrossPrime;
+		}
+
+		private IEnumerable<int> DividersOf(int number, Dictionary<int, int> primes)
+		{
+			return primes.Select(p => p.Key)
+				.TakeWhile(p => p < number)
+				.Where(p => number % p == 0);
+		}
+
+		private IEnumerable<int> PrimesLessThan(int threshold)
+		{
+			yield return 2;
+			for (int number = 3; number <= threshold; number += 2)
+			{
+				if (IsPrimeOptimised(number))
+					yield return number;
+			}
+		}
+
+		private bool IsPrimeOptimised(int number)
+		{
+			var root = Math.Sqrt(number);
+			for (int divider = 3; divider <= root; divider += 2)
+			{
+				if (number % divider == 0)
+					return false;
+			}
+
+			return true;
 		}
 	}
 }
