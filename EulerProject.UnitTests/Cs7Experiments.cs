91890b15ec8be18a75f5c9ebf1a4e5c98dda4299
ILya Lev <lis2889@gmail.com>
2018-02-11 19:29:22 +0200

    added support of C# 7.0 in msvs2015

---
+++ EulerProject.UnitTests/Cs7Experiments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EulerProject.UnitTests
+{
+	[TestClass]
+	public class Cs7Experiments
+	{
+		[TestMethod]
+		public void OutVariables_NoPriorDeclaration()
+		{
+			int baseValue = 25;
+			InitializeOutVariables(baseValue, out int square, out int root);
+
+			square.Should().Be(625);
+			root.Should().Be(5);
+		}
+
+		private void InitializeOutVariables(int baseValue, out int square, out int root)
+		{
+			square = baseValue * baseValue;
+			root = (int)Math.Sqrt(baseValue);
+		}
+	}
+}
