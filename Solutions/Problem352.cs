d94d84a13acdc7193e45359e9b9007a9db03615f
Illia Levandovskyi <lis2889@gmail.com>
2021-04-17 14:43:27 +0300

    problem 352 blood test thoughts

---
+++ Solutions/Problem352.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Solutions
+{
+    public class Problem352
+    {
+        public static double CalculateMinAvgTests(int amount, double frequency)
+        {
+            if (amount <= 0) return 0;
+            if (amount == 1) return 1;
+
+            var pos = GetAveragePositiveProbability(amount, frequency);
+            return 1 + pos * (2 * CalculateMinAvgTests(amount / 2, frequency) + (amount % 2));
+        }
+
+        private static double GetAveragePositiveProbability(int amount, double frequency)
+        {
+            return 1 - Math.Pow(1 - frequency, amount);
+        }
+    }
+}
---
+++ Tests/Problem352Tests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class Problem352Tests
+    {
+        [Fact]
+        public void CalculateMinAvgTests_25And2_2p62()
+        {
+            Problem352.CalculateMinAvgTests(25, 0.02)
+                .Should().BeApproximately(2.62279, 1e-5);
+        }
+    }
+}
